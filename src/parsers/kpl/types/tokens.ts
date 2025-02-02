9608ee813dca4ef1bcaf10b85c97742903680701
cwooper <cwooperm@gmail.com>
2025-02-02 00:38:33 -0800

    Updated tokens to accurately match cpp version

--- src/parsers/kpl/types/tokens.ts
+++ src/parsers/kpl/types/tokens.ts
@@ -1,4 +1,6 @@
-// Base token interface
+// Token Types System for the KPL Compiler
+// Adapted for TypeScript/VSCode environment
+
 export interface Token {
   type: TokenType;
   lexeme: string;
@@ -8,8 +10,17 @@ export interface Token {
   position: number;
 }
 
-// Token types enum
+// Complete enum of all token types
 export enum TokenType {
+  // Special tokens
+  EOF = "EOF",
+  ID = "ID",
+  INT_CONST = "INT_CONST",
+  DOUBLE_CONST = "DOUBLE_CONST",
+  CHAR_CONST = "CHAR_CONST",
+  STRING_CONST = "STRING_CONST",
+  OPERATOR = "OPERATOR",
+
   // Keywords
   ALLOC = "ALLOC",
   ANY_TYPE = "ANY_TYPE",
@@ -69,7 +80,7 @@ export enum TokenType {
   METHOD = "METHOD",
   METHODS = "METHODS",
   NEW = "NEW",
-  NULL = "NULL",
+  NULL_KEYWORD = "NULL",
   OF = "OF",
   PREFIX = "PREFIX",
   PTR = "PTR",
@@ -80,7 +91,7 @@ export enum TokenType {
   SELF = "SELF",
   SIZE_OF = "SIZE_OF",
   SUPER = "SUPER",
-  SUPERCLASS = "SUPERCLASS",
+  SUPER_CLASS = "SUPER_CLASS",
   SWITCH = "SWITCH",
   THROW = "THROW",
   TO = "TO",
@@ -94,61 +105,140 @@ export enum TokenType {
   VOID = "VOID",
   WHILE = "WHILE",
 
-  // Literals
-  INTEGER_LITERAL = "INTEGER_LITERAL", // e.g., 42, 0x1234ABCD
-  DOUBLE_LITERAL = "DOUBLE_LITERAL", // e.g., 3.1415, 6.022e23
-  HEX_LITERAL = "HEX_LITERAL", // e.g., 0x45454545
-  CHAR_LITERAL = "CHAR_LITERAL", // e.g., 'a', '\n'
-  STRING_LITERAL = "STRING_LITERAL", // e.g., "hello", "\t\n"
-  IDENTIFIER = "IDENTIFIER", // e.g., x, myName, MAX_SIZE
+  // Punctuation tokens
+  L_PAREN = "L_PAREN", // (
+  R_PAREN = "R_PAREN", // )
+  L_BRACE = "L_BRACE", // {
+  R_BRACE = "R_BRACE", // }
+  L_BRACK = "L_BRACK", // [
+  R_BRACK = "R_BRACK", // ]
+  COLON = "COLON", // :
+  SEMI_COLON = "SEMI_COLON", // ;
+  COMMA = "COMMA", // ,
+  PERIOD = "PERIOD", // .
+  EQUAL = "EQUAL", // =
 
-  // Operators - binary
+  // AST Node types (used in parser)
+  ALLOC_EXPR = "ALLOC_EXPR",
+  ARGUMENT = "ARGUMENT",
+  ARRAY_ACCESS = "ARRAY_ACCESS",
+  ARRAY_SIZE_EXPR = "ARRAY_SIZE_EXPR",
+  ARRAY_TYPE = "ARRAY_TYPE",
+  AS_INTEGER_EXPR = "AS_INTEGER_EXPR",
+  AS_PTR_TO_EXPR = "AS_PTR_TO_EXPR",
+  ASSIGN_STMT = "ASSIGN_STMT",
+  BOOL_CONST = "BOOL_CONST",
+  BOOL_TYPE = "BOOL_TYPE",
+  BREAK_STMT = "BREAK_STMT",
+  CALL_EXPR = "CALL_EXPR",
+  CALL_STMT = "CALL_STMT",
+  CHAR_TYPE = "CHAR_TYPE",
+  CLASS_DEF = "CLASS_DEF",
+  CLASS_FIELD = "CLASS_FIELD",
+  CLOSURE_EXPR = "CLOSURE_EXPR",
+  CONST_DECL = "CONST_DECL",
+  CONSTRUCTOR = "CONSTRUCTOR",
+  CONTINUE_STMT = "CONTINUE_STMT",
+  COUNT_VALUE = "COUNT_VALUE",
+  DO_STMT = "DO_STMT",
+  DOUBLE_TYPE = "DOUBLE_TYPE",
+  DYNAMIC_CHECK = "DYNAMIC_CHECK",
+  ERROR_DECL = "ERROR_DECL",
+  FIELD_ACCESS = "FIELD_ACCESS",
+  FIELD_INIT = "FIELD_INIT",
+  FOR_STMT = "FOR_STMT",
+  FUNCTION_PROTO = "FUNCTION_PROTO",
+  FUNCTION_TYPE = "FUNCTION_TYPE",
+  GLOBAL = "GLOBAL",
+  IF_STMT = "IF_STMT",
+  INT_TYPE = "INT_TYPE",
+  IS_INSTANCE_OF_EXPR = "IS_INSTANCE_OF_EXPR",
+  IS_KIND_OF_EXPR = "IS_KIND_OF_EXPR",
+  LOCAL = "LOCAL",
+  METHOD_PROTO = "METHOD_PROTO",
+  NAMED_TYPE = "NAMED_TYPE",
+  NEW_EXPR = "NEW_EXPR",
+  NULL_CONST = "NULL_CONST",
+  PARAMETER = "PARAMETER",
+  PTR_TYPE = "PTR_TYPE",
+  RECORD_FIELD = "RECORD_FIELD",
+  RECORD_TYPE = "RECORD_TYPE",
+  RETURN_STMT = "RETURN_STMT",
+  SELF_EXPR = "SELF_EXPR",
+  SEND_EXPR = "SEND_EXPR",
+  SEND_STMT = "SEND_STMT",
+  SIZE_OF_EXPR = "SIZE_OF_EXPR",
+  SUPER_EXPR = "SUPER_EXPR",
+  SWITCH_STMT = "SWITCH_STMT",
+  THROW_STMT = "THROW_STMT",
+  TRY_STMT = "TRY_STMT",
+  TYPE_ARG = "TYPE_ARG",
+  TYPE_DEF = "TYPE_DEF",
+  TYPE_OF_NULL_TYPE = "TYPE_OF_NULL_TYPE",
+  TYPE_PARM = "TYPE_PARM",
+  VARIABLE_EXPR = "VARIABLE_EXPR",
+  VOID_TYPE = "VOID_TYPE",
+  WHILE_STMT = "WHILE_STMT",
+  FREE_STMT = "FREE_STMT",
+  DEBUG_STMT = "DEBUG_STMT",
+
+  // Operators
+  BAR = "BAR", // |
+  CARET = "CARET", // ^
+  AMP = "AMP", // &
+  BAR_BAR = "BAR_BAR", // ||
+  AMP_AMP = "AMP_AMP", // &&
+  EQUAL_EQUAL = "EQUAL_EQUAL", // ==
+  NOT_EQUAL = "NOT_EQUAL", // !=
+  LESS = "LESS", // <
+  LESS_EQUAL = "LESS_EQUAL", // <=
+  GREATER = "GREATER", // >
+  GREATER_EQUAL = "GREATER_EQUAL", // >=
+  LESS_LESS = "LESS_LESS", // <<
+  GREATER_GREATER = "GREATER_GREATER", // >>
+  GREATER_GREATER_GREATER = "GREATER_GREATER_GREATER", // >>>
   PLUS = "PLUS", // +
   MINUS = "MINUS", // -
   STAR = "STAR", // *
   SLASH = "SLASH", // /
   PERCENT = "PERCENT", // %
-  EQUAL = "EQUAL", // =
-  EQUAL_EQUAL = "EQUAL_EQUAL", // ==
-  BANG_EQUAL = "BANG_EQUAL", // !=
-  LESS = "LESS", // <
-  LESS_EQUAL = "LESS_EQUAL", // <=
-  GREATER = "GREATER", // >
-  GREATER_EQUAL = "GREATER_EQUAL", // >=
-  AND = "AND", // &
-  OR = "OR", // |
-  XOR = "XOR", // ^
-  LOGICAL_AND = "LOGICAL_AND", // &&
-  LOGICAL_OR = "LOGICAL_OR", // ||
-  SHIFT_LEFT = "SHIFT_LEFT", // <<
-  SHIFT_RIGHT = "SHIFT_RIGHT", // >>
-  SHIFT_RIGHT_UNSIGNED = "SHIFT_RIGHT_UNSIGNED", // >>>
 
-  // Operators - unary
-  BANG = "BANG", // !
-  // STAR (*) is used both ways
-  // AND (&) is used both ways
-
-  // Punctuation
-  LEFT_PAREN = "LEFT_PAREN", // (
-  RIGHT_PAREN = "RIGHT_PAREN", // )
-  LEFT_BRACE = "LEFT_BRACE", // {
-  RIGHT_BRACE = "RIGHT_BRACE", // }
-  LEFT_BRACKET = "LEFT_BRACKET", // [
-  RIGHT_BRACKET = "RIGHT_BRACKET", // ]
-  COMMA = "COMMA", // ,
-  DOT = "DOT", // .
-  COLON = "COLON", // :
-  SEMICOLON = "SEMICOLON", // ;
-
-  // Special tokens
-  COMMENT = "COMMENT", // For both -- and /* */ comments
-  EOF = "EOF", // End of file marker
-  ERROR = "ERROR", // For lexical errors
+  // Unary operator symbols
+  UNARY_BANG = "UNARY_BANG", // !
+  UNARY_STAR = "UNARY_STAR", // *
+  UNARY_AMP = "UNARY_AMP", // &
+  UNARY_MINUS = "UNARY_MINUS", // -
 }
 
-// Keywords map for quick lookup
-export const KEYWORDS: { [key: string]: TokenType } = {
+// Map of operator symbols to their string representations
+export const OperatorStrings = {
+  [TokenType.UNARY_BANG]: "_prefix_!",
+  [TokenType.UNARY_STAR]: "_prefix_*",
+  [TokenType.UNARY_AMP]: "_prefix_&",
+  [TokenType.UNARY_MINUS]: "_prefix_-",
+  [TokenType.PLUS]: "+",
+  [TokenType.MINUS]: "-",
+  [TokenType.STAR]: "*",
+  [TokenType.SLASH]: "/",
+  [TokenType.PERCENT]: "%",
+  [TokenType.BAR]: "|",
+  [TokenType.CARET]: "^",
+  [TokenType.AMP]: "&",
+  [TokenType.BAR_BAR]: "||",
+  [TokenType.AMP_AMP]: "&&",
+  [TokenType.EQUAL_EQUAL]: "==",
+  [TokenType.NOT_EQUAL]: "!=",
+  [TokenType.LESS]: "<",
+  [TokenType.LESS_EQUAL]: "<=",
+  [TokenType.GREATER]: ">",
+  [TokenType.GREATER_EQUAL]: ">=",
+  [TokenType.LESS_LESS]: "<<",
+  [TokenType.GREATER_GREATER]: ">>",
+  [TokenType.GREATER_GREATER_GREATER]: ">>>",
+} as const;
+
+// Keywords mapping for lexer
+export const KEYWORDS: Record<string, TokenType> = {
   alloc: TokenType.ALLOC,
   anyType: TokenType.ANY_TYPE,
   array: TokenType.ARRAY,
@@ -207,7 +297,7 @@ export const KEYWORDS: { [key: string]: TokenType } = {
   method: TokenType.METHOD,
   methods: TokenType.METHODS,
   new: TokenType.NEW,
-  null: TokenType.NULL,
+  null: TokenType.NULL_KEYWORD,
   of: TokenType.OF,
   prefix: TokenType.PREFIX,
   ptr: TokenType.PTR,
@@ -218,7 +308,7 @@ export const KEYWORDS: { [key: string]: TokenType } = {
   self: TokenType.SELF,
   sizeOf: TokenType.SIZE_OF,
   super: TokenType.SUPER,
-  superclass: TokenType.SUPERCLASS,
+  superclass: TokenType.SUPER_CLASS,
   switch: TokenType.SWITCH,
   throw: TokenType.THROW,
   to: TokenType.TO,
@@ -233,15 +323,18 @@ export const KEYWORDS: { [key: string]: TokenType } = {
   while: TokenType.WHILE,
 };
 
-// Helper class for token creation
+// Helper functions for token manipulation
 export class TokenUtils {
+  /**
+   * Creates a new token with the given properties
+   */
   static createToken(
     type: TokenType,
     lexeme: string,
-    literal: any,
-    line: number,
-    column: number,
-    position: number
+    literal: any = null,
+    line: number = 1,
+    column: number = 1,
+    position: number = 0
   ): Token {
     return {
       type,
@@ -253,11 +346,90 @@ export class TokenUtils {
     };
   }
 
+  /**
+   * Checks if a given word is a keyword
+   */
   static isKeyword(word: string): boolean {
     return word in KEYWORDS;
   }
 
-  static getKeywordType(word: string): TokenType | null {
-    return KEYWORDS[word] || null;
+  /**
+   * Gets the token type for a keyword
+   */
+  static getKeywordType(word: string): TokenType | undefined {
+    return KEYWORDS[word];
+  }
+
+  /**
+   * Checks if a character could be part of an operator
+   */
+  static isOperatorChar(ch: string): boolean {
+    return /^[+\-*\\\/!@#$%^&~`|?<>=]$/.test(ch);
+  }
+
+  /**
+   * Creates a string representation of a token for debugging
+   */
+  static tokenToString(token: Token): string {
+    return `Token(type=${token.type}, lexeme="${token.lexeme}", literal=${token.literal}, line=${token.line}, col=${token.column})`;
   }
 }
+
+// Common token sets for parser error recovery and prediction
+export const StatementFirstSet = new Set<TokenType>([
+  TokenType.IF,
+  TokenType.WHILE,
+  TokenType.DO,
+  TokenType.BREAK,
+  TokenType.CONTINUE,
+  TokenType.RETURN,
+  TokenType.FOR,
+  TokenType.SWITCH,
+  TokenType.TRY,
+  TokenType.THROW,
+  TokenType.FREE,
+  TokenType.DEBUG,
+  TokenType.ID,
+  // Expression starters
+  TokenType.TRUE,
+  TokenType.FALSE,
+  TokenType.NULL_KEYWORD,
+  TokenType.SELF,
+  TokenType.SUPER,
+  TokenType.INT_CONST,
+  TokenType.DOUBLE_CONST,
+  TokenType.CHAR_CONST,
+  TokenType.STRING_CONST,
+  TokenType.FUNCTION,
+  TokenType.NEW,
+  TokenType.ALLOC,
+  TokenType.SIZE_OF,
+  TokenType.L_PAREN,
+]);
+
+export const DeclarationFirstSet = new Set<TokenType>([
+  TokenType.CONST,
+  TokenType.ERRORS,
+  TokenType.VAR,
+  TokenType.ENUM,
+  TokenType.TYPE,
+  TokenType.FUNCTIONS,
+  TokenType.INTERFACE,
+  TokenType.CLASS,
+  TokenType.BEHAVIOR,
+]);
+
+export const TypeFirstSet = new Set<TokenType>([
+  TokenType.CHAR,
+  TokenType.INT,
+  TokenType.DOUBLE,
+  TokenType.BOOL,
+  TokenType.VOID,
+  TokenType.TYPE_OF_NULL,
+  TokenType.ANY_TYPE,
+  TokenType.PTR,
+  TokenType.RECORD,
+  TokenType.ARRAY,
+  TokenType.FUNCTION,
+  TokenType.ID,
+]);
